9faab4c5f62603f328912b4bfe99cdf8a176239c
João Paulo Nascimento <65625149+JotamacLane@users.noreply.github.com>
2024-03-20 00:30:03 +0000

    Subindo

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var Parse = require('parse/node');
+const Parse = require('parse/node');
+const express = require('express')
+const app = express()
 
-Parse.initialize("ykOeEfLzm9HemrRGt1RegRl7xoFXv10qBsbM4o8a","6pMSIAVeH8uPsilJHDgebXF17DF3s7eR21Rpkigy"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+const APP_ID ="rI7MzjMSkgalJx8Me3HsfkwVuqXLH4VnNFKGgXmT";
+const JAVASCRIPT_ID ="mVaYJbkTiKvdpjgaFHrkBl4F7p3xd6HFTDIphygm";
+
+
+
+Parse.initialize(APP_ID,JAVASCRIPT_ID); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/'
+
+const Appartment= Parse.Object.extend("Appartment")
+const  appartment = new Appartment();
+const appartmentQuery = new Parse.Query(Appartment);
+app.get('/',(req,res)=>{
+    appartmentQuery.find()
+    .then((obj)=>res.json(obj))
+    .catch((err)=>res.json(err))
+})
+const port = process.env.PORT ||4000  
+app.listen(port,()=>{
+    console.log("Server is running on", port)
+})
 
 async function saveNewPerson() {
     const person = new Parse.Object("Person");
